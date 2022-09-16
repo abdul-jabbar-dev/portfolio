b32741c59aada0055ae40f488f2cbef7bc9f366d
abduljabbar15 <abduljabber1532002@gmail.com>
2022-09-16 13:03:16 +0600

    social links

--- src/Componenets/Admin/AdminComponents/EditProfile.js
+++ src/Componenets/Admin/AdminComponents/EditProfile.js
@@ -1,10 +1,32 @@
+import { Box, Container, TextField, Typography } from '@mui/material';
 import React from 'react';
 
 const EditProfile = () => {
     return (
-        <div>
-            edit profile
-        </div>
+        <Container>
+            <Box width={`50%`} sx={{ backgroundColor: '#302F4E', borderRadius: '10px', p: 6 }}>
+                <Box display={'flex '} gap={5} mb={2}>
+                    <Typography variant='h6' color={"lightGray"}>Facebook</Typography>
+                    <TextField fullWidth sx={{ input: { backgroundColor: '#353353', color: 'lightGray', fontWeight: 300 } }} variant='outlined' size='small'></TextField>
+                </Box>
+                <Box display={'flex '} gap={5} mb={2}>
+                    <Typography variant='h6' color={"lightGray"}>Github</Typography>
+                    <TextField fullWidth sx={{ input: { backgroundColor: '#353353', color: 'lightGray', fontWeight: 300 } }} variant='outlined' size='small'></TextField>
+                </Box>
+                <Box display={'flex '} gap={5} mb={2}>
+                    <Typography variant='h6' color={"lightGray"}>Instagram</Typography>
+                    <TextField fullWidth sx={{ input: { backgroundColor: '#353353', color: 'lightGray', fontWeight: 300 } }} variant='outlined' size='small'></TextField>
+                </Box>
+                <Box display={'flex  '} gap={5} mb={2}>
+                    <Typography variant='h6' color={"lightGray"}>Linkedin</Typography>
+                    <TextField fullWidth sx={{ input: { backgroundColor: '#353353', color: 'lightGray', fontWeight: 300 } }} variant='outlined' size='small'></TextField>
+                </Box>
+                <Box display={'flex  '} gap={5} mb={2}>
+                    <Typography variant='h6' color={"lightGray"}>Twitter</Typography>
+                    <TextField fullWidth sx={{ input: { backgroundColor: '#353353', color: 'lightGray', fontWeight: 300 } }} variant='outlined' size='small'></TextField>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -3,6 +3,7 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';/* import EditIcon from '@mui/icons-material/Edit'; */
 import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';/* import AssignmentIcon from '@mui/icons-material/Assignment'; */
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';/* import SettingsIcon from '@mui/icons-material/Settings'; */
+
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Box, IconButton } from '@mui/material';
 import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
@@ -23,7 +24,7 @@ const AdminNav = () => {
         <>
             <Box mt={'80px'} sx={{ display: 'flex', justifyContent: { sm: 'center', md: 'space-between', } }} alignItems={'center'}>
                 <Box></Box>
-                <Box width={{ md: '40%', sm: '80%', lg: '20%', xs: '100%' }} sx={{ display: 'flex', justifyContent: 'space-evenly', paddingY: '10px', backgroundColor: '#302F4E', borderRadius: '10px' }}>
+                <Box width={{ md: '40%', sm: '80%', lg: '20%', xs: '100%' }} sx={{ display: 'flex',alignItems:'center', justifyContent: 'space-evenly', paddingY: '10px', backgroundColor: '#302F4E', borderRadius: '10px' }}>
                     <NavLink to={'mails'}><NotificationsNoneOutlinedIcon fontSize="medium" style={{ color: 'white' }} /></NavLink>
                     <NavLink to={'projects'}><AssignmentOutlinedIcon fontSize="medium" style={{ color: 'white' }} /></NavLink>
                     <NavLink to={'settings'}><SettingsOutlinedIcon fontSize="medium" style={{ color: 'white' }} /></NavLink>
